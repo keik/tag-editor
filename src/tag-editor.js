eaebf8b525d0f41496857762bea71e6f3c53a4de
keik <k4t0.kei@gmail.com>
2016-05-09 00:14:40 +0900

    WIP

--- src/tag-editor.js
+++ src/tag-editor.js
@@ -23,23 +23,35 @@ function TagEditor(inputEl, options) {
 
   this.state = {
     // TODO define state
+    value: ''
   }
 
   this.options = objectAssign({
     // TODO define defaults
   }, options)
 
+  // TODO modular
+  this.tagsStore = []
+
   /*
    * initialize DOM elements
    */
 
   this.inputEl = inputEl
+  this.tagsContainerEl = document.createElement('ul')
+  document.body.appendChild(this.tagsContainerEl)
 
   /*
    * UI event handler
    */
 
   this.inputEl.addEventListener('keyup', _onInputKeyup.bind(this));
+
+  /*
+   * model event handler
+   */
+
+  // this.tagsStore.on('add', _handleTagsStoreAdded.bind(this))
 }
 
 /*
@@ -51,11 +63,51 @@ TagEditor.prototype.destroy = function() {
 }
 
 /*
+ * store event handlers
+ */
+
+function _handleTagsStoreAdded(tag) {
+  d('#_handleTagsStoreAdded', tag)
+  let tagEl = document.createElement('li')
+  tagEl.textContent = tag
+  this.tagsContainerEl.appendChild(tagEl)
+}
+
+function _handleTagsStoreRemoved(tag) {
+  d('#_handleTagsStoreRemoved', tag)
+  console.log('TODO')
+}
+
+/*
  * UI event handlers
  */
 
 function _onInputKeyup(e) {
   d('#_onInputKeyup')
+  let value = this.state.value = this.inputEl.value
+  if ((/ /).test(value)) {
+    _taggify.bind(this)(value)
+    this.state.value = ''
+  }
+  _render.bind(this)()
+}
+
+/*
+ * view functions
+ */
+
+function _taggify(value) {
+  d('#_taggify')
+  value.split(/ +/).forEach(function(tag) {
+    // TODO dispatch action
+    this.tagsStore.push(tag)
+    _handleTagsStoreAdded.bind(this)(tag)
+  }.bind(this))
+}
+
+function _render() {
+  d('#_render')
+  this.inputEl.value = this.state.value
 }
 
 module.exports = TagEditor
